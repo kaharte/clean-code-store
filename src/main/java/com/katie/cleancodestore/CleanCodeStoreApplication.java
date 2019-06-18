ea7d8736811a02bf2201a49ea8ee2e87c29378be
Katie Harte <katherine.harte@grainger.com>
2019-06-18 10:05:34 -0500

    First commit 🍉

---
+++ src/main/java/com/katie/cleancodestore/CleanCodeStoreApplication.java
@@ -0,0 +1,13 @@
+package com.katie.cleancodestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanCodeStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanCodeStoreApplication.class, args);
+	}
+
+}
