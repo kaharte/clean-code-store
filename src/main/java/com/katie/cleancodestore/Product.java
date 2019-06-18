5cb3d8264b9a73142df469cda8b265684e5c7f67
Katie Harte <katherine.harte@grainger.com>
2019-06-18 15:08:57 -0500

    Autowired setter injection for ProductRepository in ProductTests, Added setUp() method in tests, added new tests, overrode equals() and hashCode() methods in Product class

--- src/main/java/com/katie/cleancodestore/Product.java
+++ src/main/java/com/katie/cleancodestore/Product.java
@@ -1,8 +1,15 @@
 package com.katie.cleancodestore;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
 public class Product {
-    String name;
+
+    @Id
     String id;
+    String name;
 
     public void setName(String name) {
         this.name = name;
@@ -18,5 +25,19 @@ public class Product {
 
     public void setId(String id) {
         this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id) &&
+                Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
     }
 }
